80e99cd2bbe2277f62febf57797e6794b4c3458f
Dư Trần Vĩnh Hưng <135632758+BinOng15@users.noreply.github.com>
2025-02-17 20:18:02 +0700

    123

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Đảm bảo Tailwind quét các file này
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+export const content = [
+  "./src/**/*.{js,jsx,ts,tsx}", // Đảm bảo Tailwind quét các file này
+];
+export const theme = {
+  extend: {},
 };
+export const plugins = [];
